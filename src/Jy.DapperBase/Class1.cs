e24c946ae456f9f4b6adb03cd0c32c592b47cb9d
jjjyyy12 <jiaoyue2002@163.com>
2018-03-03 22:42:38 +0800

    dapperbase

---
+++ src/Jy.DapperBase/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Jy.DapperBase
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Jy.DapperBase/Connection/BuildConnection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jy.DapperBase.Connection
+{
+    class BuildConnection
+    {
+        //public static string GetConnectionString(string strConnectionKey)
+        //{
+        //    string strConnectionString = string.Empty;
+        //    if (ConfigurationManager.ConnectionStrings[strConnectionKey] != null)
+        //    {
+        //        strConnectionString = ConfigurationManager.ConnectionStrings[strConnectionKey].ConnectionString ?? string.Empty;
+        //    }
+        //    return strConnectionString;
+        //}
+        //public static IDbConnection GetConnection(string dbType, string cinemaID)
+        //{
+        //    IDbConnection connection;
+        //    switch (dbType)
+        //    {
+        //        case "ORACLE":
+        //            connection = new OracleConnection(GetConnectionString("dp_" + cinemaID));
+        //            break;
+        //        default:
+        //            connection = new OracleConnection(GetConnectionString("dp_" + cinemaID));
+        //            break;
+        //    }
+        //    connection.Open();
+        //    return connection;
+        //}
+        //public static IDbConnection GetConnection(string dbType)
+        //{
+        //    return GetConnection(dbType, "Entities");
+        //}
+    }
+}
