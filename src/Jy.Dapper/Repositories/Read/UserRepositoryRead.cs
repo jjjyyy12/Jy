bf6e8d0f2d05007e43b4257a46a6a13c4ee15bb0
jjjyyy12 <jiaoyue2002@163.com>
2018-04-04 17:58:41 +0800

    UserRepositoryRead

--- src/Jy.Dapper/Repositories/Read/UserRepositoryRead.cs
+++ src/Jy.Dapper/Repositories/Read/UserRepositoryRead.cs
@@ -1,9 +1,9 @@
 ﻿using Jy.DapperBase;
 using Jy.DapperBase.Repositories;
+using Jy.DapperBase.Repositories.Extensions;
 using Jy.Domain.Entities;
 using Jy.Domain.IRepositories; 
-using Jy.IRepositories;
-using Microsoft.Extensions.Options;
+using Jy.IRepositories; 
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,11 +31,11 @@ namespace Jy.Dapper.Repositories
         /// <returns>存在返回用户实体，否则返回NULL</returns>
         public UserIndex CheckUserIndex(string userName, string password)
         {
-            return base.GetAllList<UserIndex>((u) => { u => u.UserName == userName && u.Password == password}).FirstOrDefault();
+            return dbContext.connection.Query<UserIndex>(u => u.UserName == userName && u.Password == password).FirstOrDefault();
         }
         public List<UserIndex> GetUserIndexList(Expression<Func<UserIndex, bool>> predicate)
         {
-            return dbContext.UserIndexs.Where(predicate).ToList();
+            return dbContext.connection.Query(predicate).ToList();
         }
         /// <summary>
         /// 检查用户是存在
@@ -45,9 +45,9 @@ namespace Jy.Dapper.Repositories
         /// <returns>存在返回用户实体，否则返回NULL</returns>
         public User CheckUser(string userName, string password)
         {
-            var query = from u in dbContext.Users
+            var query = from u in _dbContext.connection.Query<User>()
                         where u.UserName == userName && u.Password == password
-                        join x in dbContext.Departments on u.DepartmentId equals x.Id into temp
+                        join x in _dbContext.connection.Query<Department>() on u.DepartmentId equals x.Id into temp
                         from x in temp.DefaultIfEmpty()
                         select new User()
             {
@@ -60,7 +60,7 @@ namespace Jy.Dapper.Repositories
                 ,Department = x
             };
             var dd = query.Cast<User>().FirstOrDefault();
-            var rr = from ur in dbContext.UserRoles where ur.UserId == dd.Id select ur;
+            var rr = from ur in _dbContext.connection.Query<UserRole>() where ur.UserId == dd.Id select ur;
                 dd.UserRoles = rr.ToList();
             return dd;
 
