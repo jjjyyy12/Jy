4af3a1458d8e3db7b988080afcfe0c8f73794dcd
jjjyyy12 <jiaoyue2002@163.com>
2018-06-02 12:49:10 +0800

    CKafkaExtension

---
+++ src/Jy.CKafka/CKafkaExtension.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Jy.IMessageQueue;
+using Microsoft.Extensions.Configuration;
+using Jy.CKafka.Implementation;
+
+namespace Jy.CKafka
+{
+    public static class CKafkaExtension
+    {
+        public static IServiceCollection AddCKafkaServices(this IServiceCollection services)
+        {
+            var options = new KafkaOptions();
+            services.AddSingleton(options);
+            AppConfig.KafkaConfig = options.GetConfig();
+
+            //services.AddScoped<IKafkaPersisterConnection, KafkaProducerPersistentConnection>();
+            services.AddScoped<IQueueOperationSubscriptionsManager, InMemorySubscriptionsManager>();
+            return services.AddSingleton<IBigQueueOperation, QueueOperationCKafka>();
+        }
+
+        public static IServiceCollection AddCKafkaServices(this IServiceCollection services, KafkaOptions options)
+        {
+            services.AddSingleton(options);
+            AppConfig.KafkaConfig = options.GetConfig();
+             
+            services.AddScoped<IQueueOperationSubscriptionsManager, InMemorySubscriptionsManager>();
+            return services.AddSingleton<IBigQueueOperation, QueueOperationCKafka>();
+        }
+        public static IServiceCollection AddCKafkaServices(this IServiceCollection services, IConfigurationRoot Configuration)
+        {
+            var options = new KafkaOptions()
+            {
+                Servers = Configuration.GetSection("KafkaConfig").GetValue<string>("BorkerList")
+            };
+            services.AddSingleton(options);
+            AppConfig.KafkaConfig = options.GetConfig();
+
+            services.AddScoped<IQueueOperationSubscriptionsManager, InMemorySubscriptionsManager>();
+            return services.AddSingleton<IBigQueueOperation, QueueOperationCKafka>();
+        }
+    }
+}
---
+++ src/Jy.CKafka/Implementation/KafkaOptions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jy.CKafka.Implementation
+{
+    public class KafkaOptions
+    {
+        public string Servers { get; set; } = "127.0.0.1";
+
+        public int MaxQueueBuffering { get; set; } = 10;
+
+        public int MaxSocketBlocking { get; set; } = 10;
+
+        public bool EnableAutoCommit { get; set; } = false;
+
+        public bool LogConnectionClose { get; set; } = false;
+
+        public KafkaOptions Value => this;
+
+        public IEnumerable<KeyValuePair<string, object>> GetConfig()
+        {
+            var configs = new List<KeyValuePair<string, object>>();
+            configs.Add(new KeyValuePair<string, object>("bootstrap.servers", Servers));
+            configs.Add(new KeyValuePair<string, object>("queue.buffering.max.ms", MaxQueueBuffering.ToString()));
+            configs.Add(new KeyValuePair<string, object>("socket.blocking.max.ms", MaxSocketBlocking.ToString()));
+            configs.Add(new KeyValuePair<string, object>("enable.auto.commit", EnableAutoCommit.ToString()));
+            configs.Add(new KeyValuePair<string, object>("log.connection.close", LogConnectionClose.ToString()));
+            return configs;
+        }
+    }
+}
